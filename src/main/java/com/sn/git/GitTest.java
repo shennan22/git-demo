cf2f0cc29f935b2b31509a0b31179a16065b05c2
shennan <1295649607@qq.com>
2021-05-18 15:18:15 +0800

    第一次提交

---
+++ src/main/java/com/sn/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.sn.git;
+
+/**
+ * @author sn
+ * @date 2021/5/18 15:16
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("版本1");
+    }
+}
