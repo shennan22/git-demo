9f3f9730e1e120c3304dbba4437789353b663cd5
shennan <1295649607@qq.com>
2021-05-18 15:27:31 +0800

    第一次提交 hot-fix

--- src/main/java/com/sn/git/GitTest.java
+++ src/main/java/com/sn/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("版本1");
         System.out.println("版本2");
+        System.out.println("版本3");
     }
 }
